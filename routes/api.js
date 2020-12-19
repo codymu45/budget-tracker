c8a09744f424a72b69fa5f4f1403a01c4e8acc24
Cody Mullen <mullen.cody@yahoo.com>
2020-12-19 09:39:05 -0800

    changing router back to app

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const Transaction = require("../models/transaction.js");
 
-router.get('/',(req,res) => {
+app.get('/',(req,res) => {
   return res.send('Hello');
   });
 
